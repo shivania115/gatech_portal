32f79f2d0e90dcf34bd1e43519db35661810fb5c
Lisa Sun <lisa.sun@emory.edu>
2020-12-04 16:14:07 -0500

    Updates

--- precache-manifest.54f87cf8fcf23edd2f7b14e2367150b0.js
+++ precache-manifest.54f87cf8fcf23edd2f7b14e2367150b0.js
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": "/gatech_portal/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/gatech_portal/static/media/icons.8e3c7f55.eot"
-  },
-  {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/gatech_portal/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/gatech_portal/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "faff92145777a3cbaf8e7367b4807987",
@@ -40,40 +40,40 @@ self.__precacheManifest = [
     "url": "/gatech_portal/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/gatech_portal/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/gatech_portal/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/gatech_portal/static/media/outline-icons.cd6c777f.woff2"
   },
   {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/gatech_portal/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/gatech_portal/static/media/outline-icons.ad97afd3.ttf"
-  },
-  {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/gatech_portal/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/gatech_portal/static/media/outline-icons.ad97afd3.ttf"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/gatech_portal/static/media/outline-icons.82f60bd0.svg"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/gatech_portal/static/media/brand-icons.13db00b7.eot"
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/gatech_portal/static/media/brand-icons.a046592b.woff"
   },
   {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/gatech_portal/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "a046592bac8f2fd96e994733faf3858c",
-    "url": "/gatech_portal/static/media/brand-icons.a046592b.woff"
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/gatech_portal/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gatech_portal/precache-manifest.717dbbe1ff98a46d557edda7930db21f.js"
+  "/gatech_portal/precache-manifest.54f87cf8fcf23edd2f7b14e2367150b0.js"
 );
 
 workbox.clientsClaim();
