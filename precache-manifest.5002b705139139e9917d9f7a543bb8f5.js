e3183cacbcab5ea2b441dac6e68c6382e9cd8f73
Lisa Sun <lisa.sun@emory.edu>
2020-12-10 00:44:20 -0500

    Updates

--- precache-manifest.5002b705139139e9917d9f7a543bb8f5.js
+++ precache-manifest.5002b705139139e9917d9f7a543bb8f5.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "ed093899671fc4d51f65",
+    "revision": "6713024094f9c1b9842d",
     "url": "/gatech_portal/static/css/main.d957b437.chunk.css"
   },
   {
-    "revision": "ed093899671fc4d51f65",
-    "url": "/gatech_portal/static/js/main.e28d085a.chunk.js"
+    "revision": "6713024094f9c1b9842d",
+    "url": "/gatech_portal/static/js/main.f57744d1.chunk.js"
   },
   {
     "revision": "2069c063a8ec61de4959",
@@ -20,52 +20,56 @@ self.__precacheManifest = [
     "url": "/gatech_portal/static/js/2.c3d78af3.chunk.js"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/gatech_portal/static/media/icons.8e3c7f55.eot"
-  },
-  {
-    "revision": "0ab54153eeeca0ce03978cc463b257f7",
-    "url": "/gatech_portal/static/media/icons.0ab54153.woff2"
-  },
-  {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/gatech_portal/static/media/icons.faff9214.woff"
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/gatech_portal/static/media/flags.9c74e172.png"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/gatech_portal/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/gatech_portal/static/media/outline-icons.cd6c777f.woff2"
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/gatech_portal/static/media/icons.0ab54153.woff2"
   },
   {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/gatech_portal/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/gatech_portal/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/gatech_portal/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/gatech_portal/static/media/outline-icons.cd6c777f.woff2"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
     "url": "/gatech_portal/static/media/outline-icons.701ae6ab.eot"
   },
   {
-    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
-    "url": "/gatech_portal/static/media/outline-icons.ad97afd3.ttf"
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/gatech_portal/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/gatech_portal/static/media/icons.962a1bf3.svg"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/gatech_portal/static/media/brand-icons.13db00b7.eot"
-  },
-  {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/gatech_portal/static/media/outline-icons.82f60bd0.svg"
   },
   {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/gatech_portal/static/media/brand-icons.13db00b7.eot"
+  },
+  {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/gatech_portal/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/gatech_portal/static/media/outline-icons.ad97afd3.ttf"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -80,11 +84,7 @@ self.__precacheManifest = [
     "url": "/gatech_portal/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/gatech_portal/static/media/flags.9c74e172.png"
-  },
-  {
-    "revision": "24eaea548127df7f2398de59e8945087",
+    "revision": "ae0b75257daae1d67b991d63a48705e5",
     "url": "/gatech_portal/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gatech_portal/precache-manifest.90e9a5e1e0246110067f7a4adf189d1b.js"
+  "/gatech_portal/precache-manifest.5002b705139139e9917d9f7a543bb8f5.js"
 );
 
 workbox.clientsClaim();
